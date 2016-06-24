0cc3f7e23fef54b4984ac4fa817a8de51c4fe51f
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2016-06-24 15:39:48 +0300

    requests to Google Geo API implemented

--- WSD-10.py
+++ WSD-10.py
@@ -1,15 +1,14 @@
 import sqlalchemy
 
 from flask import Flask
-from sqlalchemy import Table, ForeignKey, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import requests
+import json
 
 Base = declarative_base()
-
-# TODO to git-Bitbucket this project
-
 engine = sqlalchemy.create_engine('postgres://postgres@/postgres')
 conn = engine.connect()
 conn.execute('commit')
@@ -18,12 +17,17 @@ conn.execute('commit')
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    employees = relationship('Employee', secondary='department_employee')
+    name = Column(String)
+    geolocation = relationship('Geolocation', secondary='users_geolocations')
 
 
 class Geolocation(Base):
     __tablename__ = 'geolocations'
     id = Column(Integer, primary_key=True)
+    city = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    user = relationship('User', secondary='users_geolocations')
 
 
 class UserGeolocation(Base):
@@ -32,18 +36,42 @@ class UserGeolocation(Base):
     geolocation_id = Column(Integer, ForeignKey('geolocations.id'), primary_key=True)
 
 
-session = sessionmaker()
-session.configure(bind=engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
 Base.metadata.create_all(engine)
 
+users = {
+    'Vitaly Zdanevich': 'Minsk',
+    'Vladimir Posner': 'Moscow',
+    'Yuri Burlan': 'New York',
+    'Michael Laitman': 'Bnei Brak',
+    'Pavel Durov': 'Saint Petersburg'
+}
+
+for user in users:
+    city = users[user]
+    req = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+city)
+    jsn = json.loads(req.text)
+    lat = jsn['results'][0]['geometry']['location']['lat']
+    lng = jsn['results'][0]['geometry']['location']['lng']
+
+    new_user = User(name=user)
+    city = Geolocation(city=users[user], latitude=lat, longitude=lng)
+
+    new_user.geolocation.append(city)
+    session.add(new_user, city)
+
+session.commit()
+session.close()
 conn.close()
 
 
-    # app = Flask(__name__)
-    #
-    # @app.route('/')
-    # def hello_world():
-    #     return 'Hello World!'
-    #
-    # if __name__ == '__main__':
-    #     app.run()
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+# if __name__ == '__main__':
+#     app.run()
